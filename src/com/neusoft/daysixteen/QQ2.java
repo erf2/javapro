7a5dcf8db2936a19d2bf0731ca063c60e1001089
erf2 <2535532134@qq.com>
2020-02-16 19:30:00 +0800

    你好，请翻阅！

--- src/com/neusoft/daysixteen/QQ2.java
+++ src/com/neusoft/daysixteen/QQ2.java
@@ -12,11 +12,11 @@ public class QQ2 implements Runnable {
         List list = new ArrayList();
         for (int i=0;i<10000;i++){
             list.add("我爱你第"+i+"次，亲爱的");
-            System.out.println("我爱你第"+i+"次，亲爱的");
+            System.out.println(Thread.currentThread().getName()+"我爱你第"+i+"次，亲爱的");
             if (list.size()==10000){
-                System.out.println("哈，爱你一万次哦");
+                System.out.println(Thread.currentThread().getName()+"哈，爱你一万次哦");
             }else{
-                System.out.println("不够，再爱一次");
+                System.out.println(Thread.currentThread().getName()+"不够，再爱一次");
             }
         }
         System.out.println("我爱了"+list.size()+"次");
--- src/com/neusoft/daysixteen/QQ2Test.java
+++ src/com/neusoft/daysixteen/QQ2Test.java
@@ -8,5 +8,11 @@ public class QQ2Test {
         QQ2 qq2 = new QQ2();
         Thread t = new Thread(qq2);
         t.start();
+        QQ2 qq21 = new QQ2();
+        Thread t2 = new Thread(qq21);
+        t2.start();
+        QQ2 qq22 = new QQ2();
+        Thread t3 = new Thread(qq22);
+        t3.start();
     }
 }
