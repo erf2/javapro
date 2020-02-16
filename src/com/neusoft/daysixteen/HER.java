9a497d76e296fc20690a0fa6c7b476b521aec005
erf2 <2535532134@qq.com>
2020-02-16 23:16:54 +0800

    你好，请翻阅！

--- src/com/neusoft/daysixteen/HER.java
+++ src/com/neusoft/daysixteen/HER.java
@@ -1,20 +1,26 @@
 package com.neusoft.daysixteen;
 
 import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.Semaphore;
 
 /**
  * Created by lan_jia_nao on 2020/2/16.
  */
-public class RFT683 {
+public class HER {
     public static int a;
-    public static Object obj = new Object();
+
     public static void main(String[] args) {
-        CountDownLatch c = new CountDownLatch(5000);
-        for (int i=0;i<5000;i++){
+        Semaphore semaphore = new Semaphore(8);
+        CountDownLatch c = new CountDownLatch(3000);
+        for (int i=0;i<3000;i++){
             Thread t = new Thread(()->{
-                synchronized(obj){
+                try {
+                    semaphore.acquire(5);
                     a++;
                     c.countDown();
+                    semaphore.release(5);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
                 }
             });
             t.start();
