e5c4ac1b44b0cdfc40e9373931ef9ebac3717687
erf2 <2535532134@qq.com>
2020-02-19 08:38:24 +0800

    你好，请翻阅！

---
+++ src/com/neusoft/dayeighteen/IUI998.java
@@ -0,0 +1,39 @@
+package com.neusoft.dayeighteen;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+/**
+ * Created by lan_jia_nao on 2020/2/18.
+ */
+public class IUI998 {
+    public static void main(String[] args) {
+        String url = "jdbc:mysql://192.168.13.131:3306/erf";
+        String username = "root";
+        String password = "123456";
+        Connection con = null;
+        PreparedStatement pstmt = null;
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            con = DriverManager.getConnection(url,username,password);
+            pstmt = con.prepareStatement("create database ooo");
+            int i = pstmt.executeUpdate();
+            System.out.println(i);
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }finally{
+            try {
+                if (pstmt!=null)
+                    pstmt.close();
+                if (con!=null)
+                    con.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
