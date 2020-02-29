d4cbf67e673eb18995f0607a0d66671afbfa3ec8
erf2 <2535532134@qq.com>
2020-02-29 22:44:00 +0800

    你好，请翻阅！

---
+++ src/com/neusoft/daytwenty_nine/ZWHZ.java
@@ -0,0 +1,13 @@
+package com.neusoft.daytwenty_nine;
+
+import com.sun.org.apache.xerces.internal.dom.PSVIAttrNSImpl;
+
+/**
+ * Created by lan_jia_nao on 2020/2/29.
+ */
+public class ZWHZ {
+    public static void main(String[] args) {
+        char c = '汉';
+        System.out.println(c);
+    }
+}
